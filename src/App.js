e2ed93075386d906244646921f90cbe367189ffa
Bipul Kumar Singh <bksingh@netlink.com>
2022-03-18 19:42:16 +0530

    code for first example

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learning React"/>
+      <Todo text="Check value"/>
+      <Todo text="This is good thing"/>
+    </div>
+  );
+}
+
+export default App;
